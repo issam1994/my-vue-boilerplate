e53b9a2d22729fcee1e397706d34ff7fa5351324
issam1994 <issamaitouahmane@gmail.com>
2020-03-07 11:41:36 +0100

    installed & configured Tailwindcss, the autoprefixer & purgecss.\ncreated a few empty pages and linked them top the router. \ninstalled axios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import '@/assets/css/tailwindcss.css'
 import router from "./router";
 import store from "./store";
 
---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-gray-200">
+        <button class="px-4 py-2 bg-white shadow">Log in</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
