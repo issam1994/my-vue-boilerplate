2b76d4499d27828f597a8556ddb48d6479e7c0de
issam1994 <issamaitouahmane@gmail.com>
2020-03-15 12:14:26 +0100

    added postcss-nesting & postcss-import & linked them to postcss config.

--- postcss.config.js
+++ postcss.config.js
@@ -14,7 +14,9 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
 module.exports = {
     plugins: [
+        require('postcss-import'),
         require('tailwindcss'),
+        require('postcss-nesting'),
         require('autoprefixer'),
         ...process.env.NODE_ENV === 'production'
             ? [purgecss]
