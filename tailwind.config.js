e3b2a2cb347bcab14a26f09bc07d803ad0c62d65
issam1994 <issamaitouahmane@gmail.com>
2020-03-19 12:45:55 +0100

    updated tailwindcss config file: by adding a gradients pligin example. extended the spacing utilities. extended font size utilities.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,38 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        ...(() => {
+          const spacing = {}
+          for (let i = 0; i <= 128; i++) {
+            spacing[`${i}`] = (0.25 * i) + 'rem'
+          }
+          console.log("updated spacing utilities")
+          return spacing
+        })()
+      },
+      fontSize: {
+        app: {
+          sm: ".75rem",
+          md: "1rem",
+          lg: "1.25rem",
+          xl: "1.50rem",
+        }
+      }
+    },
   },
   variants: {},
   plugins: [
     require('@tailwindcss/custom-forms'),
+    plugin(({ addUtilities, theme }) => {
+      const gradients = {
+        '.gradient': {
+          background: `linear-gradient(to right, ${theme('colors.red.600')},${theme('colors.green.600')})`
+        },
+      }
+      addUtilities(gradients)
+    })
   ],
 }
